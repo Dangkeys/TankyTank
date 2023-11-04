04758ff129707ad07c844ccd5cb2b6dcdfe7ee4f
Dangkeys <TajdangZaa@gmail.com>
2023-11-04 16:33:37 +0700

    Added health component

--- Assets/Player/ProjectileLauncher.cs
+++ Assets/Player/ProjectileLauncher.cs
@@ -13,22 +13,36 @@ public class ProjectileLauncher : NetworkBehaviour
     [SerializeField] Transform projectileSpawnPoint;
     [SerializeField] GameObject serverProjectilePrefab;
     [SerializeField] GameObject clientProjectilePrefab;
+    [SerializeField] GameObject muzzleFlash;
+    [SerializeField] Collider2D playerCollider;
     [Header("Settings")]
     [SerializeField] float projectileSpeed = 40f;
+    [SerializeField] float fireRate;
+    [SerializeField] float muzzleFlashDuration;
     bool shouldFire;
+    float previousFireTime;
+    float muzzleFlashTimer;
     // Start is called before the first frame update
     public override void OnNetworkSpawn()
     {
         if (!IsOwner) return;
         inputReader.PrimaryFireEvent += HandlePrimaryFire;
     }
-    
+
     void Update()
     {
+        if (muzzleFlashTimer > 0f)
+        {
+            muzzleFlashTimer -= Time.deltaTime;
+            if (muzzleFlashTimer <= 0f)
+                muzzleFlash.SetActive(false);
+        }
         if (!IsOwner) return;
         if (!shouldFire) return;
+        if(Time.time < (1/fireRate) + previousFireTime) return;
         PrimaryFireServerRpc(projectileSpawnPoint.position, projectileSpawnPoint.up);
         SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        previousFireTime = Time.time;
     }
 
 
@@ -47,19 +61,30 @@ public class ProjectileLauncher : NetworkBehaviour
     {
         GameObject projectileInstance = Instantiate(serverProjectilePrefab, spawnPos, Quaternion.identity);
         projectileInstance.transform.up = direction;
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = rb.transform.up * projectileSpeed;
+        }
         SpawnDummyProjectileClientRpc(spawnPos, direction);
     }
     [ClientRpc]
     private void SpawnDummyProjectileClientRpc(Vector3 spawnPos, Vector3 direction)
     {
         if (IsOwner) return;
-        GameObject projectileInstance = Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
-        projectileInstance.transform.up = direction;
+        SpawnDummyProjectile(spawnPos, direction);
     }
     private void SpawnDummyProjectile(Vector3 spawnPos, Vector3 direction)
     {
+        muzzleFlash.SetActive(true);
+        muzzleFlashTimer = muzzleFlashDuration;
         GameObject projectileInstance = Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
         projectileInstance.transform.up = direction;
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = rb.transform.up * projectileSpeed;
+        }
     }
 
 }
--- Assets/Projectiles/DestroySelfOnContract.cs
+++ Assets/Projectiles/DestroySelfOnContract.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroySelfOnContract : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         Destroy(gameObject);
     }
 }
