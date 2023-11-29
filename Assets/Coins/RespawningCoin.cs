5c1aa3105a860ec117ddbe5d2e95b697f92dfd57
Dangkeys <TajdangZaa@gmail.com>
2023-11-29 21:14:34 +0700

    Created map design

--- Assets/Coins/RespawningCoin.cs
+++ Assets/Coins/RespawningCoin.cs
@@ -1,15 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RespawningCoin : Coin
 {
+    public event Action<RespawningCoin> OnCollected;
+    Vector3 previousPosition;
+    private void Update() {
+        if(previousPosition != transform.position)
+        {
+            Show(true);
+        }
+        previousPosition = transform.position;
+    }
     public override int Collect()
     {
-        if(!IsServer) return 0;
-        if(isCollected) return 0;
+        if (!IsServer)
+        {
+            Show(false);
+            return 0;
+        }
+
+
+        if (isCollected) return 0;
         isCollected = true;
+        OnCollected?.Invoke(this);
         return coinValue;
     }
 
+    internal void Reset()
+    {
+        isCollected = false;
+    }
 }
