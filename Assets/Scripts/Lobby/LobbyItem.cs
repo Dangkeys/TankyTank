ac3aacf47d8229f764f0a7980af6b2a037bb5c19
Dangkeys <TajdangZaa@gmail.com>
2023-12-17 09:00:01 +0700

    Implemented lobby

---
+++ Assets/Scripts/Lobby/LobbyItem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyItem : MonoBehaviour
+{
+    [SerializeField] private Button joinButton;
+    [SerializeField] private TMP_Text lobbyNameText;
+    [SerializeField] private TMP_Text lobbyPlayersText;
+    private LobbiesList lobbiesList;
+    private Lobby lobby;
+    private void Awake()
+    {
+        joinButton.onClick.AddListener(Join);
+    }
+    public void Initialise(LobbiesList lobbiesList,Lobby lobby)
+    {
+        this.lobbiesList = lobbiesList;
+        lobbyNameText.text = lobby.Name;
+        lobbyPlayersText.text = $"{lobby.Players.Count}/{lobby.MaxPlayers}";
+    }
+    private void Join()
+    {
+        lobbiesList.JoinAsync(lobby);
+    }
+}
--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Lobbies.Models;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -12,6 +14,7 @@ using UnityEngine.SceneManagement;
 
 public class HostGameManager
 {
+    private string lobbyId;
     private const string GAME_SCENE = "Game";
     private Allocation allocation;
     private String joinCode;
@@ -37,11 +40,44 @@ public class HostGameManager
             Debug.Log(e);
             return;
         }
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject(
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode
+                    )
+                }
 
+            };
+            Lobby lobby =
+            await Lobbies.Instance.CreateLobbyAsync("My Lobby", MAX_CONNECTIONS, lobbyOptions);
+            lobbyId = lobby.Id;
+            HostSingleton.Instance.StartCoroutine(HeartBeatLobby(15));
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+            return;
+        }
        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
        RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
        transport.SetRelayServerData(relayServerData);
        NetworkManager.Singleton.StartHost();
        NetworkManager.Singleton.SceneManager.LoadScene(GAME_SCENE, LoadSceneMode.Single);
     }
+    private IEnumerator HeartBeatLobby(float waitTimeSeconds)
+    {
+        WaitForSecondsRealtime delay = new  WaitForSecondsRealtime(waitTimeSeconds);;
+        while(true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+        }
+    }
 }
+
